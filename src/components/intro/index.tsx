dd69bacfa834548d2e0713c5285092b3dd3fe666
chrismcardle <chris@hauntdigital.co.nz>
2024-10-14 14:55:46 +1300

    update intro firstname value and improve intro layout

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import * as styles from './intro.module.scss';
 
 export const Intro = () => {
+	const username = { FirstName: 'John', LastName: 'Smith' };
 	return (
-		<div>
-			<p>Welcome user.firstname</p>
+		<div className={styles.intro}>
+			<p>Welcome {username.FirstName}</p>
 		</div>
 	);
 };
