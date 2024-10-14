d8019bf958c16d7a62323f61522e6c5b9c7fe597
chrismcardle <chris@hauntdigital.co.nz>
2024-10-14 14:42:27 +1300

    menu toggle state setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,21 @@
-import React, { FC } from 'react';
+import React, { useState, FC } from 'react';
 import * as styles from './app.module.scss';
 import { Header } from './components/header';
 import { Intro } from './components/intro';
 import { Form } from './components/form';
 
 export const App = () => {
+	const [overlayStatus, setOverlayStatus] = useState(false);
+
+	const handleStatusChange = (newStatus) => {
+		setOverlayStatus(newStatus);
+	};
 	return (
 		<>
 			<h1 className={styles.demo}>Welcome to your technical test!</h1>
 			<div>
-				<Header />
+				{overlayStatus ? 'open' : 'closed'}
+				<Header onStatusChange={handleStatusChange} />
 				<Intro />
 				<Form />
 			</div>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as styles from './header.module.scss';
 import { NavOverlay } from '../navOverlay';
 
-export const Header = () => {
+export const Header = ({ onStatusChange }) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleStatus = () => {
+		const newStatus = !isOpen;
+		setIsOpen(newStatus);
+		onStatusChange(newStatus);
+	};
 	return (
 		<div>
-			<div>Toggle</div>
-			<NavOverlay />
+			<div>
+				<a
+					href='#'
+					role='menubutton'
+					aria-label={isOpen ? 'close main menu' : 'open main menu'}
+					aria-expanded={isOpen ? 'true' : 'false'}
+					onClick={toggleStatus}
+					className={`${styles.menuLink}  ${
+						isOpen ? `${styles.menuOpen}` : ''
+					}`}
+				>
+					<i className={styles.hamburger} aria-hidden='true'></i>
+				</a>
+			</div>
+			{isOpen && <NavOverlay />}
 			<h1>Register card form</h1>
 		</div>
 	);
