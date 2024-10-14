f86728d078aa577b1c0f5d00eb8f0781b29f23a7
chrismcardle <chris@hauntdigital.co.nz>
2024-10-14 14:18:54 +1300

    init commit

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from "react";
+import * as styles from "./app.module.scss";
+
+export const App = () => {
+  return <h1 className={styles.demo}>Welcome to your technical test!</h1>;
+};
